2ccdaeae076b7f904b73ddb4d3d4d178656ed48c
AmusingThrone <amusingthrone@gmail.com>
2017-05-18 19:42:47 -0700

    Updated to download Kitsune

--- setup.php
+++ setup.php
@@ -1,10 +1,80 @@
 <?php
-echo 'The config setup is for Kitsune AS2 only!';
- 
+echo 'Welcome to the Kitsune Auto-Setup';
+
+$url         = "https://github.com/AmusingThrone/kitsune-auto-setup/raw/master/Kitsune.zip";
+$zipFile     = "Kitsune.zip"; // zip file
+$zipResource = fopen($zipFile, "w");
+$ch          = curl_init();
+
+echo 'Downloading Server...';
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FAILONERROR, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_FILE, $zipResource);
+$page = curl_exec($ch);
+if (!$page) {
+    echo "Error :- " . curl_error($ch);
+}
+curl_close($ch);
+
+$zip         = new ZipArchive;
+$extractPath = "kitsune";
+if ($zip->open($zipFile) != "true") {
+    echo "Error :- Unable to extract Server";
+}
+
+$zip->extractTo($extractPath);
+$zip->close();
+
 //config starts.
 
-$answers = array('yes', 'y', 'yeah', 'sure', 'ya', 'YES', 'Yes', 'Yeah', 'Sure', 'Ya', 'YEAH', 'SURE', 'ok', 'okay', 'Ok', 'Okay', 'OK', 'OKAY', '1', 'Y', 'yes!', 'k', 'kk', 'K', 'KK');
-$no = array('no','nah','nope','No','NO','0','n','noo','Nope','no u', 'NO!');
+$answers = array(
+    'yes',
+    'y',
+    'yeah',
+    'sure',
+    'ya',
+    'YES',
+    'Yes',
+    'Yeah',
+    'Sure',
+    'Ya',
+    'YEAH',
+    'SURE',
+    'ok',
+    'okay',
+    'Ok',
+    'Okay',
+    'OK',
+    'OKAY',
+    '1',
+    'Y',
+    'yes!',
+    'k',
+    'kk',
+    'K',
+    'KK'
+);
+$no      = array(
+    'no',
+    'nah',
+    'nope',
+    'No',
+    'NO',
+    '0',
+    'n',
+    'noo',
+    'Nope',
+    'no u',
+    'NO!'
+);
 
 
 echo "Enter Database Host: ";
@@ -13,66 +83,69 @@ echo "Enter Database Username: ";
 $dbUser = trim(fgets(STDIN));
 echo "Enter Database Password: ";
 $dbPass = trim(fgets(STDIN));
- 
+
 if ($dbPass == "") {
     echo "Without db pass? Omg, please setup a password for your db.";
 }
 
- 
 if (strlen($dbPass) < 5) {
     echo "Um, please use a strong password.";
 }
+
 echo "Enter Database Name: ";
 $dbName = trim(fgets(STDIN));
-$con   = mysqli_connect($dbhost, $dbUser, $dbPass);
+$con    = mysqli_connect($dbhost, $dbUser, $dbPass);
 
 if (!$con) {
-echo "Oops! We couldn't connect to the database..\n";
-echo "Continue anyway? ";
-$conan = trim(fgets(STDIN));
-} else {
-    $xml = new DOMDocument('1.0', 'utf-8');
-    $xml->formatOutput = true;
-    $xml->preserveWhiteSpace = false;
-    $xml->load('Database.xml');
-
-    $xml->getElementsByTagName('name')->item(0)->nodeValue = $dbName;
-    $xml->getElementsByTagName('address')->item(0)->nodeValue = $dbhost;
-    $xml->getElementsByTagName('username')->item(0)->nodeValue = $dbUser;
-    $xml->getElementsByTagName('password')->item(0)->nodeValue = $dbPass;
-
-    $xml->save("Database.xml");
-    echo 'Successfully updated your Database.xml file!';
-}    
-
-if (in_array($conan, $answers)) {
+    echo "Oops! We couldn't connect to the database..\n";
+    echo "Continue anyway? ";
     $conan = trim(fgets(STDIN));
-    $xml = new DOMDocument('1.0', 'utf-8');
-    $xml->formatOutput = true;
+} else {
+    $xml                     = new DOMDocument('1.0', 'utf-8');
+    $xml->formatOutput       = true;
     $xml->preserveWhiteSpace = false;
-    $xml->load('Database.xml');
-
-    $xml->getElementsByTagName('name')->item(0)->nodeValue = $dbName;
-    $xml->getElementsByTagName('address')->item(0)->nodeValue = $dbhost;
+    $xml->load('kitsune\Database.xml');
+    
+    $xml->getElementsByTagName('name')->item(0)->nodeValue     = $dbName;
+    $xml->getElementsByTagName('address')->item(0)->nodeValue  = $dbhost;
     $xml->getElementsByTagName('username')->item(0)->nodeValue = $dbUser;
     $xml->getElementsByTagName('password')->item(0)->nodeValue = $dbPass;
-
-    $xml->save("Database.xml");
+    
+    $xml->save("kitsune\Database.xml");
     echo 'Successfully updated your Database.xml file!';
 }
- 
+
+if (in_array($conan, $answers)) {
+    $conan                   = trim(fgets(STDIN));
+    $xml                     = new DOMDocument('1.0', 'utf-8');
+    $xml->formatOutput       = true;
+    $xml->preserveWhiteSpace = false;
+    $xml->load('kitsune\Database.xml');
+    
+    $xml->getElementsByTagName('name')->item(0)->nodeValue     = $dbName;
+    $xml->getElementsByTagName('address')->item(0)->nodeValue  = $dbhost;
+    $xml->getElementsByTagName('username')->item(0)->nodeValue = $dbUser;
+    $xml->getElementsByTagName('password')->item(0)->nodeValue = $dbPass;
+    
+    $xml->save("kitsune\Database.xml");
+    echo 'Successfully updated your Database.xml file!';
+}
+
 if (in_array($conan, $no)) {
     $conan = trim(fgets(STDIN));
     die("Shutting down configuration...");
 }
- 
+
 echo "Almost done!\n";
 echo "Would you like us to setup the database?";
+echo "It's recommended you do, otherwise you will have to do this manually later";
+
 $answer = trim(fgets(STDIN));
 if (in_array($answer, $no)) {
     $conan = trim(fgets(STDIN));
     die("Shutting down configuration...");
 }
+
 mysqli_query($con, 'CREATE DATABASE IF NOT EXISTS ' . $dbName . '');
 mysqli_query($con, 'USE ' . $dbName . '');
 mysqli_query($con, "CREATE TABLE IF NOT EXISTS `bans` (
@@ -84,7 +157,7 @@ mysqli_query($con, "CREATE TABLE IF NOT EXISTS `bans` (
   `Time` int(8) NOT NULL,
   `Type` smallint(3) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;");
-   
+
 mysqli_query($con, "CREATE TABLE IF NOT EXISTS `igloos` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Owner` int(10) unsigned NOT NULL,
@@ -94,7 +167,7 @@ mysqli_query($con, "CREATE TABLE IF NOT EXISTS `igloos` (
   `Furniture` text NOT NULL,
   `Locked` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;");
-   
+
 mysqli_query($con, "CREATE TABLE IF NOT EXISTS `penguins` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Username` char(12) NOT NULL,
@@ -127,7 +200,7 @@ mysqli_query($con, "CREATE TABLE IF NOT EXISTS `penguins` (
   `Ignores` text NOT NULL,
   `MinutesPlayed` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;");
-   
+
 mysqli_query($con, "CREATE TABLE IF NOT EXISTS `postcards` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Recipient` int(10) unsigned NOT NULL,
@@ -138,7 +211,7 @@ mysqli_query($con, "CREATE TABLE IF NOT EXISTS `postcards` (
   `Type` smallint(5) unsigned NOT NULL,
   `HasRead` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;");
-   
+
 mysqli_query($con, "CREATE TABLE IF NOT EXISTS `puffles` (
 `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Owner` int(10) unsigned NOT NULL,
@@ -150,7 +223,7 @@ mysqli_query($con, "CREATE TABLE IF NOT EXISTS `puffles` (
   `Rest` tinyint(3) unsigned NOT NULL DEFAULT '100',
   `Walking` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;");
-   
+
 mysqli_query($con, "INSERT INTO `puffles` (`ID`, `Owner`, `Name`, `AdoptionDate`, `Type`, `Food`, `Play`, `Rest`, `Walking`) VALUES
 (3, 101, 'Blue', 1453750614, 0, 100, 100, 100, 0),
 (4, 101, 'Blue', 1453752421, 0, 100, 100, 100, 0),
@@ -158,9 +231,10 @@ mysqli_query($con, "INSERT INTO `puffles` (`ID`, `Owner`, `Name`, `AdoptionDate`
 (6, 101, 'Yellow', 1453753887, 6, 100, 100, 100, 0),
 (7, 101, 'Pink', 1453829330, 1, 100, 100, 100, 0),
 (8, 101, 'Purple', 1454159945, 4, 100, 100, 100, 0);");
- 
+
 mysqli_close($con);
- 
+
 echo "The database is now setup!";
+echo "Kitsune Setup!";
 
 ?>
